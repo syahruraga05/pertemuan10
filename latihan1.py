53ebf6eb1e0b7e12e2f71c6d5d45a1e6abfc773d
syahru <syahruragaramdhani@gmail.com>
2020-12-03 16:28:00 +0700

    Menambahkan File Python

---
+++ latihan1.py
@@ -0,0 +1,44 @@
+# mengubah function menggunakan lambda
+import math as m
+
+def a(x):
+	return x ** 2
+
+
+lambda x: x ** 2
+
+print("1. Mengubah function menggunakan Lambda \n   def a(x): \n \t   return x ** 2")
+print("   Hasil : lambda x: x ** 2")
+
+
+def b(x, y):
+	return math.sqrt(x ** 2 + y ** 2)
+
+
+lambda x, y: m.sqrt(x ** 2 + y ** 2)
+
+print(35*"=")
+print("2. Mengubah function menggunakan Lambda \n   def b(x, y): \n \t   return math.sqrt(x ** 2 + y ** 2)")
+print("   Hasil : lambda x, y: math.sqrt(x ** 2 + y ** 2))")
+
+
+def c(*args):
+    return sum(args) / len(args)
+
+
+lambda *args: sum(args) / len(args)
+
+print(35*"=")
+print("3. Mengubah function menggunakan Lambda \n   def c(*args): \n \t   return sum(args) / len(args)")
+print("   Hasil : lambda *args: sum(args) / len(args))")
+
+
+def d(s):
+   	return "".join(set(s))
+
+
+lambda s: "".join(set(s))
+
+print(35*"=")
+print("4. Mengubah function menggunakan Lambda \n   def d(s): \n \t   return "".join(set(s))")
+print("   Hasil : lambda s: "".join(set(s)))")
