3e4c9ea946ff888d0908486df9431456d8ab4c9e
syahru <syahruragaramdhani@gmail.com>
2020-12-03 18:25:45 +0700

    Menambahkan file python

---
+++ praktikum.py
@@ -0,0 +1,93 @@
+data = {}
+
+def tambah():
+    print("Tambah Data")
+    nama    = input("Nama\t\t: ")
+    nim     = int(input("NIM\t\t: "))
+    tugas   = int(input("Nilai Tugas\t: "))
+    uts     = int(input("Nilai UTS\t: "))
+    uas     = int(input("Nilai UAS\t: "))
+    nilaiakhir = (tugas * 0.3 + uts * 0.35 + uas * 0.35)
+    data[nama] = nim, tugas, uts, uas, nilaiakhir
+
+
+def tampilkan():
+    if data.items():
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~| Daftar Nilai |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("_______________________________________________________________________________________")
+        print("|  No  |      NAMA     |        NIM        |    TUGAS   |   UTS   |   UAS   | AKHIR   |")
+        print("|______|_______________|___________________|____________|_________|_________|_________|")
+        i = 0
+        for a in data.items():
+            i += 1
+            print(f"| {i:4} | {a[0]:13s} | {a[1][0]:17} | {a[1][1]:10d} |  {a[1][2]:6d} | {a[1][2]:7d} | {a[1][4]:6.2f} | ")
+    else:
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~| Daftar Nilai |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("________________________________________________________________________________________")
+        print("|  No  |      Nama     |      NIM      |   TUGAS  |   UTS   |   UAS   | Nilai Akhir    |")
+        print("________________________________________________________________________________________")
+        print("|      |               |             Tidak Ada Data         |         |                |")
+        print("________________________________________________________________________________________")
+
+
+def hapus():
+    print("Hapus Data Nilai Mahasiswa")
+    nama = input(" Masukan Nama\t:")
+    if nama in data.keys():
+        del data[nama]
+        print()
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("===| BERHASIL MENGHAPUS DATA |===")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    else:
+        print("Data {0} tidak ada".format(nama))
+
+
+def ubah():
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("===| Edit Data Nilai Mahasiswa |===")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    nama = input("Masukan Nama\t\t: ")
+    print("___________________________________")
+    if nama in data.keys():
+        nim = input("NIM baru\t\t: ")
+        tugas = int(input("Nilai Tugas Baru\t: "))
+        uts = int(input("Nilai UTS Baru\t\t: "))
+        uas = int(input("Nilai UAS Baru\t\t: "))
+        nilaiakhir = (tugas * 30 / 100 + uts * 35 / 100 + uas * 35 / 100)
+        data[nama] = nim, tugas, uts, uas, nilaiakhir
+        print()
+        print("<><><><><><><><><><><><><><><><>")
+        print("====| BERHASIL MENGUBAH DATA |====")
+        print("<><><><><><><><><><><><><><><><>")
+    else:
+        print("Data nilai {0} tidak ada ".format(nama))
+
+
+while True:
+    print("")
+    print("|_<><><><><><><><><><><><><><><><><>_|")
+    print("|~~~~~~~~| DATA MAHASISWA |~~~~~~~~~~|")
+    print("|_<><><><><><><><><><><><><><><><><>_|")
+    x = input("1.| Tambah Data \n2.| Tampilkan Data \n3.| Hapus Data \n4.| Ubah Data \n0.| Keluar Aplikasi \nPilih menu : ")
+    if x.lower() == "1":
+        tambah()
+    elif x.lower() == "2":
+        tampilkan()
+    elif x.lower() == "3":
+        hapus()
+    elif x.lower() == "4":
+        ubah()
+    elif x.lower() == "0":
+        print()
+        print("<><><><><><><><><><><><><><><><>")
+        print("====== KELUAR DARI PROGRAM ======")
+        print("<><><><><><><><><><><><><><><><>")
+        break
+
+    else:
+        print()
+        print("<><><><><><><><><><><><><><><><>")
+        print("== Pilihan Anda Tidak Tersedia ==")
+        print("== Pilihlah Menu Yang Tersedia ==")
+        print("<><><><><><><><><><><><><><><><>")
